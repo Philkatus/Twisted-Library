e74abc43d3fd9bf37a7ea74f8beefb99f0184834
Hildebrandt <philiphi@web.de>
2021-04-13 12:30:01 +0200

    [added] Pathcreator

---
+++ Assets/Scripts/Abstract/State.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State
+{
+    public virtual void initialize()
+    {
+
+    }
+
+    public virtual void Movement()
+    {
+
+    }
+
+    public virtual void Jump()
+    {
+
+    }
+
+    public virtual void Snap() 
+    {
+
+    }
+
+    public virtual void GroundDetection() 
+    {
+
+    }
+
+    public virtual void Finish() 
+    {
+
+    }
+}
