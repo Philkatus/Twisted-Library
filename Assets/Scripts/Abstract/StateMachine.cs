213499a18bd5d9fe150057dca047cf2c53827a87
Hildebrandt <philiphi@web.de>
2021-04-13 13:07:23 +0200

    [added] basic Statemachine implementation

---
+++ Assets/Scripts/Abstract/StateMachine.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StateMachine :MonoBehaviour
+{
+    protected PlayerState State;
+
+    public void SetState(PlayerState state) 
+    {
+        State = state;
+        StartCoroutine(State.initialize());
+    }
+
+
+}
