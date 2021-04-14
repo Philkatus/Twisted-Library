6919ae9e3f8185ea1ac05261d0aea17ab75ac1a8
Daria <eleonora.pankau@gmail.com>
2021-04-14 10:57:39 +0200

    [changed] PlayerWalking

--- Assets/Scripts/Player/Movement/states/PlayerWalking.cs
+++ Assets/Scripts/Player/Movement/states/PlayerWalking.cs
@@ -4,9 +4,54 @@ using UnityEngine;
 
 public class PlayerWalking : PlayerState
 {
-    public PlayerWalking(PlayerMovementStateMachine playerStateMachine)
-        : base(playerStateMachine) 
+
+    public PlayerWalking(PlayerMovementStateMachine playerStateMachine) : base(playerStateMachine)
+    {
+        PlayerStateMachine = playerStateMachine;
+    }
+
+    public override IEnumerator initialize()
     {
 
+        yield return null;
+    }
+
+    public override void Movement()
+    {
+        Vector3 direction = new Vector3(PlayerStateMachine.FowardInput, 0, PlayerStateMachine.SideWardsInput);
+        PlayerStateMachine.controller.Move(direction * Time.deltaTime * PlayerStateMachine.speed);
+        if (direction != Vector3.zero)
+        {
+            PlayerStateMachine.controller.transform.parent.transform.forward = direction;
+        }
+        PlayerStateMachine.CheckForShelf();
+    }
+
+    public override void Jump()
+    {
+        while (true)
+        {
+            if (Input.GetButtonDown("Jump") && PlayerStateMachine.controller.isGrounded)
+            {
+                //onFall.trigger
+            }
+        }
+    }
+
+    public override IEnumerator Snap()
+    {
+        while (true)
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+
+            }
+            yield return null;
+        }
+    }
+
+    public override IEnumerator Finish()
+    {
+        yield return null;
     }
 }
