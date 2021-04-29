1377db34a6894a805d9fa047264dac39cb55307f
Maria Fleischer <mariafleischer111@gmail.com>
2021-04-29 18:06:41 +0200

    [added] an actually working base-swing

--- Assets/Scripts/Player/Movement/states/PlayerSwinging.cs
+++ Assets/Scripts/Player/Movement/states/PlayerSwinging.cs
@@ -36,15 +36,14 @@ public class PlayerSwinging : PlayerSliding
     private float tensionForce = 0f;
     private float gravityForce = 0f;
 
-
     // Keep track of the current velocity
     Vector3 currentVelocity = new Vector3();
 
     // We use these to smooth between values in certain framerate situations in the `Update()` loop
     Vector3 currentStatePosition;
     Vector3 previousStatePosition;
-
-    float ladderLength; 
+    Vector3 playerVelocity;
+    float ladderLength;
 
     public override void Initialize()
     {
@@ -53,22 +52,20 @@ public class PlayerSwinging : PlayerSliding
         pSM.swingingPosition = 0;
         base.Initialize();
         closestShelf = pSM.closestShelf;
+        playerVelocity = Vector3.zero;
 
-        /*//new try
         ladderLength = pSM.ladder.GetComponent<LadderSizeStateMachine>().ladderLength;
         Pivot = pSM.ladder.gameObject; //ist ein gameObject, weil sich der Pivot ja verschiebt, wenn man slidet
-        
+
         Bob = Pivot.transform.GetChild(1).gameObject;
         Bob.transform.position = pSM.ladder.transform.position + -pSM.ladderDirection * ladderLength;
-
+        Bob.transform.parent = null;
         bobStartingPosition = Bob.transform.position;
         bobStartingPositionSet = true;
-        PendulumInit();*/
+        PendulumInit();
     }
 
-    float t = 0f;
     float dt = 0.01f;
-    float currentTime = 0f;
     float accumulator = 0f;
 
     public override void Movement()
@@ -83,174 +80,34 @@ public class PlayerSwinging : PlayerSliding
         VertexPath path = pSM.closestShelf.pathCreator.path;
         Vector3 SwingingDirection = pSM.ladderMesh.forward;
         Vector3 swingingAxis = pSM.ladder.right;
-        #region first try
-        /*
-        pSM.playerVelocity -= SwingingDirection * stats.SwingingGravity * Mathf.Sin(SwingingDistance) * Time.deltaTime;
-            
+
+        #region PendulumTry
 
 
-        pSM.playerVelocity -= SwingingDirection * stats.SwingingAcceleration * pSM.swingingInput * Time.deltaTime * Time.deltaTime*10 ;
-        Debug.Log(SwingingDirection + " "+pSM.playerVelocity.normalized +" "+ pSM.resultingSpeed(pSM.playerVelocity.normalized, SwingingDirection));
-        if (pSM.resultingSpeed(pSM.playerVelocity.normalized, SwingingDirection) <= 0)
-        {
-            pSM.playerVelocity -= SwingingDirection * stats.SwingingAcceleration * pSM.swingingInput * Time.deltaTime;
-                
-
-        }
-        //pSM.playerVelocity += SwingingDirection * stats.SwingingDeceleration * Mathf.Sin(SwingingDistance) * Time.deltaTime;
-
-        float swingingVelocity = pSM.resultingSpeed(pSM.playerVelocity, Vector3.up);
-        Debug.DrawRay(pSM.transform.position, pSM.resultingVelocity(pSM.playerVelocity, Vector3.up),Color.black);
-        pSM.playerVelocity -= pSM.resultingVelocity(pSM.playerVelocity,Vector3.up);
-        swingingVelocity = Mathf.Clamp(swingingVelocity * (10000-stats.SwingingDeceleration)/10000, -stats.maxSwingSpeed, stats.maxSwingSpeed);
-        pSM.playerVelocity += swingingVelocity * Vector3.up;
-        pSM.swingingPosition -= swingingVelocity;
-        Quaternion targetRotation = Quaternion.AngleAxis(-swingingVelocity, pSM.ladder.right);
-        pSM.ladder.rotation = targetRotation * pSM.ladder.rotation;
-
-        
-        pSM.swingingPosition = Mathf.Repeat(pSM.swingingPosition,360);
-        SwingingDistance = Vector3.Distance(pSM.ladder.transform.position+ pSM.ladderDirection, pSM.ladder.transform.position+ Vector3.up) ;
-
-        */
-        #endregion
-        #region second try
-        /*
-        float PotentialEnergy=0;
-        float KinetikEnergy = 0;
-        float TotalEnergy = 0;
-        float maximumSwingEnergy=20;
-        TotalEnergy += pSM.swingingInput * stats.SwingingAcceleration * Time.deltaTime;
-        TotalEnergy = Mathf.Clamp(TotalEnergy, 0, maximumSwingEnergy);
-        KinetikEnergy = TotalEnergy - PotentialEnergy;
-
-        if (pSM.SwinginForwards) 
-        {
-            pSM.playerVelocity -= SwingingDirection *stats.SwingingAcceleration  * Time.deltaTime;
-
-            if (pSM.resultingSpeed(SwingingDirection, Vector3.down) >= 0)
-            {
-                KinetikEnergy += stats.SwingingAcceleration * Time.deltaTime;
-                PotentialEnergy = TotalEnergy - KinetikEnergy;
-            }
-            else if (pSM.resultingSpeed(SwingingDirection, Vector3.down) < 0)
-            {
-                PotentialEnergy += stats.SwingingAcceleration * Time.deltaTime;
-                KinetikEnergy = TotalEnergy - PotentialEnergy;
-            }
-
-        }
-        else  
-        {
-            pSM.playerVelocity += SwingingDirection *stats.SwingingAcceleration  * Time.deltaTime;
-
-            if (pSM.resultingSpeed(SwingingDirection, Vector3.down) < 0)
-            {
-                KinetikEnergy += stats.SwingingAcceleration * Time.deltaTime;
-                PotentialEnergy = TotalEnergy - KinetikEnergy;
-            }
-            else if (pSM.resultingSpeed(SwingingDirection, Vector3.down) >= 0)
-            {
-                PotentialEnergy += stats.SwingingAcceleration * Time.deltaTime;
-                KinetikEnergy = TotalEnergy - PotentialEnergy;
-            }
-        }
-        
-        
-        #endregion
-
-        #region MariaTry 
-        /*
-        //nimmt den y Wert der Leiter im Einheitskreis (wert liegt zwischen -1 & 1)
-        float relativeHeight = -pSM.ladderDirection.normalized.y;
-
-        //remap relative height from 1 -> (-1) to 1 -> 0.1 
-        relativeHeight = (relativeHeight - 1) / (-1 - 1) * (0.1f - 1) + 1;
-
-        //relative height liegt zwischen 0.1 & 1, Gravity zwischen 0 & 1: Je h�her die Relative H�he, umso langsamer die Gesamgtgeschwindigkeit
-        float gravityMultiplier = relativeHeight * stats.SwingingGravity;
-
-        //nimmt aus der Geradigen Bewegung einen float force: wie viel Kraft hat die Bewegung gerade?
-        currentMovementForce = pSM.playerVelocity.magnitude / (SwingingDirection.magnitude * Time.deltaTime);
-        Debug.Log(pSM.playerVelocity.magnitude + " / " + SwingingDirection.magnitude + " = " + currentMovementForce);
-        //currentMovementForce = Mathf.Clamp(currentMovementForce, 0, stats.maxSwingSpeed);
-
-        //Wenn es vorne oder hinten nicht mehr weitergeht WIP
-        float thisInput = pSM.swingingInput;
-        if (thisInput == 0 && previousInput != 0 || thisInput == 1 && previousInput != 1)
-        {
-            //Wechsele Schwingrichtung aus und pass auf, dass sie nicht sofort zur�ckwechselt?
-            currentMovementDirection *= -1;
-        }
-        //Bei einem Input 
-        pSM.playerVelocity = SwingingDirection * pSM.swingingInput * stats.SwingingAcceleration;
-
-        Vector3 toRestingPoint = Vector3.down - -pSM.ladderDirection.normalized;
-        toRestingPoint = toRestingPoint.normalized * 0.1f;
-        //Bei keinem Input
-        if (pSM.swingingInput == 0)
-        {
-            //Deceleration
-            pSM.playerVelocity += (toRestingPoint * stats.SwingingDeceleration);
-            Debug.DrawRay(pSM.transform.position, toRestingPoint * stats.SwingingDeceleration, Color.red); //Deceleration Force
-        }
-        //Gravity 
-        pSM.playerVelocity += toRestingPoint * gravityMultiplier;
-
-        // PlayerForce & Bewegung in die Richtige Richtung
-        pSM.playerVelocity += currentMovementDirection * -10 * SwingingDirection; // * currentMovementForce;
-
-        //Debugs
-        Debug.DrawRay(pSM.transform.position, pSM.playerVelocity, Color.blue); //playerVelocity
-        Debug.DrawRay(pSM.transform.position, (currentMovementDirection * -10) * SwingingDirection, Color.green); //Constant Movement* currentMovementForce
-        Debug.DrawRay(pSM.transform.position, SwingingDirection * pSM.swingingInput * stats.SwingingAcceleration, Color.black); //Input Movement
-        Debug.DrawRay(pSM.transform.position, toRestingPoint * gravityMultiplier, Color.yellow); //Gravity Movement
-
-        // * Time Deltatime so it doesnt get lost on accident
-        pSM.playerVelocity *= Time.deltaTime;
-
-        //apply the rotation to the ladder
-        Vector3 axis = pSM.closestShelf.pathCreator.bezierPath.GetPoint(0) - pSM.closestShelf.pathCreator.bezierPath.GetPoint(1);
-        Vector3 outwardVector = pSM.ladderMesh.transform.position - pSM.ladder.transform.position;
-        float swingingVelocity = Vector3.SignedAngle(outwardVector, outwardVector + pSM.playerVelocity, axis);
-
-        //remap swingingVelocity from -180 -> 180 to .stats.maxSwingSpeed -> stats.maxSwingSpeed
-        swingingVelocity = (swingingVelocity + 180) / (180 + 180) * (stats.maxSwingSpeed + stats.maxSwingSpeed) - stats.maxSwingSpeed;
-        Quaternion targetRotation = Quaternion.AngleAxis(swingingVelocity, -pSM.ladder.right);
-        pSM.ladder.rotation = targetRotation * pSM.ladder.rotation;
-
-        //
-        previousInput = thisInput;
-        */
-        #endregion
-       /* #region PendulumTry
-
-        //Wie viel Zeit vergeht zwischen 2 Frames? Warum nicht time.deltatime??
-        float frameTime = Time.time - currentTime;
-        currentTime = Time.time;
-
+        float frameTime = Time.deltaTime;
         accumulator += frameTime;
-
-        //immer wenn accumulator 0.01f ist, startet er eine while schleife, die die neue current Position berechnet. Das macht er (dt/accumulator)-Mal
+        // immer wenn accumulator 0.01f ist, startet er eine while schleife, die die neue current Position berechnet. Das macht er (accumulator/dt)-Mal
         while (accumulator >= dt)
         {
             previousStatePosition = currentStatePosition;
-            currentStatePosition = PendulumUpdate(currentStatePosition, dt);
-            //integrate(state, this.t, this.dt);
+            currentStatePosition = PendulumUpdate(currentStatePosition);
             accumulator -= dt;
-            t += dt;
         }
-
         float alpha = accumulator / dt;
-
         Vector3 newPosition = currentStatePosition * alpha + previousStatePosition * (1f - alpha);
-        //Nicht Bob.transform.position sondern die Leiter nach da rotieren
-        //this.Bob.transform.position = newPosition;
+
+        //die Leiter korrekt rotieren
         Vector3 axis = pSM.ladder.right;
-        float rotateByAngle = Vector3.SignedAngle(previousStatePosition, newPosition + pSM.playerVelocity, axis);
-        Quaternion targetRotation = Quaternion.AngleAxis(rotateByAngle, -pSM.ladder.right);
+        float rotateByAngle = (Vector3.SignedAngle(- pSM.ladderDirection, newPosition - pSM.ladder.transform.position, axis));
+        Debug.Log(rotateByAngle);
+        //rotateByAngle = Mathf.Clamp(rotateByAngle, -stats.maxSwingSpeed, stats.maxSwingSpeed);
+        Quaternion targetRotation = Quaternion.AngleAxis(rotateByAngle, axis);
         pSM.ladder.rotation = targetRotation * pSM.ladder.rotation;
-        #endregion */
+
+        //move Bob
+        Bob.transform.position = newPosition;
+        
+        #endregion
     }
     void PendulumInit()
     {
@@ -269,50 +126,72 @@ public class PlayerSwinging : PlayerSliding
     }
 
 
-    Vector3 PendulumUpdate(Vector3 currentStatePosition, float deltaTime)
+    Vector3 PendulumUpdate(Vector3 currentStatePosition)
     {
+        Vector3 inputForce = Vector3.zero;
+
         // Erstellt eine Gravity und addiert sie auf die currentVelocity
         gravityForce = mass * stats.SwingingGravity;
         gravityDirection = Physics.gravity.normalized;
-        currentVelocity += gravityDirection * gravityForce * deltaTime;
+        currentVelocity += gravityDirection * gravityForce * dt;
+        Debug.DrawRay(Bob.transform.position, gravityDirection * gravityForce * dt, Color.green, Time.deltaTime);
 
         Vector3 pivot_p = Pivot.transform.position;
         Vector3 bob_p = this.currentStatePosition;
 
-        //berechnet, wie weit Pivot & bob voneinander entfernt w�ren, wenn die Gravity straight auf den Bob addiert wird
-        Vector3 auxiliaryMovementDelta = currentVelocity * deltaTime;
+        //berechnet, wie weit Pivot & bob voneinander entfernt waeren, wenn die Gravity straight auf den Bob addiert wird
+        Vector3 auxiliaryMovementDelta = currentVelocity * dt;
         float distanceAfterGravity = Vector3.Distance(pivot_p, bob_p + auxiliaryMovementDelta);
 
-        // If at the end of the rope: Technisch wollen wir sowas nicht, weil der Punkt immer am Ende des Seils ist/sein sollen ? m�ssen wir iwie �ndern 
-        if (distanceAfterGravity > this.ropeLength || Mathf.Approximately(distanceAfterGravity, ropeLength))
-        {
-            //Vector richtung pivot;
-            tensionDirection = (pivot_p - bob_p).normalized;
-            //dreht tension direction um 90 Grad auf der x-achse, setzt y auf null & normalized sie
-            pendulumSideDirection = (Quaternion.Euler(0f, 90f, 0f) * tensionDirection);
-            pendulumSideDirection.Scale(new Vector3(1f, 0f, 1f));
-            pendulumSideDirection.Normalize();
-            //nimmt das negative Kreuzprodukt
-            tangentDirection = (-1f * Vector3.Cross(tensionDirection, pendulumSideDirection)).normalized;
-            // Winkel zwischen Gravity Direction & Vektor -> Bob
-            float inclinationAngle = Vector3.Angle(bob_p - pivot_p, gravityDirection);
-            //Gravityst�rke * Cos(inclinationAngle)
-            tensionForce = mass * stats.SwingingGravity * Mathf.Cos(Mathf.Deg2Rad * inclinationAngle);
-            float centripetalForce = ((mass * Mathf.Pow(currentVelocity.magnitude, 2)) / ropeLength);
-            tensionForce += centripetalForce;
 
-            currentVelocity += tensionDirection * tensionForce * deltaTime;
+        // If at the end of the rope: Technisch wollen wir sowas nicht, weil der Punkt immer am Ende des Seils ist/sein sollen ? m�ssen wir iwie �ndern 
+        //if (distanceAfterGravity > this.ropeLength || Mathf.Approximately(distanceAfterGravity, ropeLength))
+        //{
+        //Vector richtung pivot;
+        tensionDirection = (pivot_p - bob_p).normalized;
+
+        //dreht tension direction um 90 Grad auf der x-achse, setzt y auf null & normalized sie
+        pendulumSideDirection = (Quaternion.Euler(0f, 90f, 0f) * tensionDirection);
+        pendulumSideDirection.Scale(new Vector3(1f, 0f, 1f));
+        pendulumSideDirection.Normalize();
+
+        //nimmt das negative Kreuzprodukt => Tangente
+        tangentDirection = (-1f * Vector3.Cross(tensionDirection, pendulumSideDirection)).normalized;
+
+        // Winkel zwischen Gravity Direction & Vektor -> Bob
+        float inclinationAngle = Vector3.Angle(bob_p - pivot_p, gravityDirection);
+
+        //Gravitystaerke * Cos(inclinationAngle)
+        tensionForce = mass * stats.SwingingGravity * Mathf.Cos(Mathf.Deg2Rad * inclinationAngle);
+        float centripetalForce = ((mass * Mathf.Pow(currentVelocity.magnitude, 2)) / ropeLength);
+
+        tensionForce += centripetalForce;
+        currentVelocity += tensionDirection * tensionForce * dt;
+
+        //Deceleration 
+        currentVelocity = Vector3.Lerp(currentVelocity, Vector3.zero, dt * stats.SwingingDeceleration * (1 - pSM.swingingInput));
+
+        //Acceleration
+        if (pSM.swingingInput != 0 && Mathf.Abs(Vector3.Dot(-tangentDirection, Bob.transform.forward)) >= .97f)
+        {
+            inputForce = Bob.transform.forward * stats.SwingingAcceleration * pSM.swingingInput * dt;
         }
+
+        playerVelocity = Bob.transform.forward * pSM.resultingSpeed(Bob.transform.forward, currentVelocity);
+
+
+        //Debug.DrawRays
+        Debug.DrawRay(Bob.transform.position, tensionDirection * tensionForce * dt, Color.red, Time.deltaTime);
+        Debug.DrawRay(Bob.transform.position + pSM.transform.up * 0.1f, currentVelocity, Color.cyan, Time.deltaTime);
+        Debug.DrawRay(Bob.transform.position, inputForce, Color.black, Time.deltaTime);
+        Debug.DrawRay(Bob.transform.position, tangentDirection, Color.blue, Time.deltaTime);
+        Debug.DrawRay(Bob.transform.position, playerVelocity, Color.white, Time.deltaTime);
+        //}
 
         // Get the movement delta
         Vector3 movementDelta = Vector3.zero;
-        movementDelta += currentVelocity * deltaTime;
-
-
-        //Ist das Seil zu lang? return currentStatePosition + movementDelta
-        float distance = Vector3.Distance(pivot_p, currentStatePosition + movementDelta);
-        //bekomm den punkt ropelength oder distance in movementrichtung
-        return this.GetPointOnLine(pivot_p, currentStatePosition + movementDelta, distance <= this.ropeLength ? distance : this.ropeLength);
+        movementDelta += (playerVelocity + inputForce) * dt;
+        return GetPointOnLine(pivot_p, currentStatePosition + movementDelta, ropeLength);
     }
 
     Vector3 GetPointOnLine(Vector3 start, Vector3 end, float distanceFromStart)
