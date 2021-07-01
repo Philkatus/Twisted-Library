f6fa64f4032b1b969cf31e6f3cd1d682f95bd13d
Felix Szczesny <felix@maschinen-mensch.com>
2021-07-01 16:27:04 +0200

    [added] a lot of anims in scene

--- Assets/Scripts/Challenges/Behaviours/VolcanoChallengeMiddlelBehaviour.cs
+++ Assets/Scripts/Challenges/Behaviours/VolcanoChallengeMiddlelBehaviour.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class VolcanoChallengeMiddlelBehaviour : MonoBehaviour, ICentralObject
 {
+
+    [SerializeField] Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,6 @@ public class VolcanoChallengeMiddlelBehaviour : MonoBehaviour, ICentralObject
 
     public void OnAllComponentsCompleted()
     {
-
+        animator.SetBool("isActivated", true);
     }
 }
