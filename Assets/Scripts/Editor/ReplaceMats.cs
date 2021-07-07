e24370dde2e020b09f6b95fe2afb570187c24540
Maria Fleischer <mariafleischer111@gmail.com>
2021-07-07 21:21:32 +0200

    [added] my most risky update YET: updated (almost) all rail materials.

--- Assets/Scripts/Editor/ReplaceMats.cs
+++ Assets/Scripts/Editor/ReplaceMats.cs
@@ -7,7 +7,7 @@ public class ReplaceMats : EditorWindow
 {
     [SerializeField] private Material fromMat;
     [SerializeField] private Material toMat;
-    [SerializeField] private List<GameObject> prefabsToChange = new List<GameObject>();
+    //[SerializeField] private List<GameObject> prefabsToChange = new List<GameObject>();
 
     [MenuItem("Tools/Replace Mats")]
     static void CreateReplaceWithMats()
@@ -17,39 +17,47 @@ public class ReplaceMats : EditorWindow
 
     private void OnGUI()
     {
+        /*
         ScriptableObject target = this;
         SerializedObject so = new SerializedObject(target);
         SerializedProperty stringsProperty = so.FindProperty("prefabsToChange");
-
+        */
         fromMat = (Material)EditorGUILayout.ObjectField("From Material", fromMat, typeof(Material), false);
         toMat = (Material)EditorGUILayout.ObjectField("To Material", toMat, typeof(Material), false);
-        EditorGUILayout.PropertyField(stringsProperty, true);
-        so.ApplyModifiedProperties();
+        //EditorGUILayout.PropertyField(stringsProperty, true);
+        //so.ApplyModifiedProperties();
 
         if (GUILayout.Button("Replace Missing Rails"))
         {
-            MeshRenderer[] allMeshRenderers = GameObject.FindObjectsOfType<MeshRenderer>();
+            int meshIndex = 0;
+            MeshRenderer[] allMeshRenderers = FindObjectsOfType<MeshRenderer>();
+            List<int> meshIndices = new List<int>();
+            List<int> matIndices = new List<int>();
+
             foreach (MeshRenderer mr in allMeshRenderers)
             {
                 List<int> allChangedIndices = new List<int>();
                 int index = 0;
                 foreach (Material m in mr.sharedMaterials)
                 {
-                    if (m == fromMat)
+                    if (m.name == "Yellow")
                     {
-                        allChangedIndices.Add(index);
+                        meshIndices.Add(meshIndex);
+                        matIndices.Add(index);
                     }
                     index++;
                 }
-                foreach (int i in allChangedIndices)
-                {
-                    mr.sharedMaterials[i] = toMat;
-                }
-
+                meshIndex++;
+            }
+            for (int i = 0; i < matIndices.Count; i++)
+            {
+                int j = meshIndices[i];
+                int k = matIndices[i];
+                allMeshRenderers[j].material = toMat;
             }
             GUI.enabled = false;
         }
-        if (GUILayout.Button("Replace All Selected Prefabs"))
+        /*if (GUILayout.Button("Replace All Selected Prefabs"))
         {
             foreach (GameObject prefab in prefabsToChange)
             {
@@ -77,5 +85,6 @@ public class ReplaceMats : EditorWindow
 
         }
         GUI.enabled = false;
+    }*/
     }
 }
